7bd1ed22731d61c3dbac160e176c48a55c14daa7
Jair Aviles <jair.aviles@segment.com>
2022-08-21 13:37:31 -0700

    Add more page steps

--- __tests__/browser.test.ts
+++ __tests__/browser.test.ts
@@ -4,13 +4,38 @@ import sleep from '../src/lib/sleep';
 describe('My first test', () => {
 
   it('Should open and close browser', async () => {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null
+    })
 
     const page = await browser.newPage()
-    await page.goto('http://automationpractice.com/index.php')
-    sleep(1000)
+    await page.goto('https://yahoo.com/')
+    // sleep(5000)
+    await page.waitForSelector('img')
+
+    // Reload page
+    await page.reload()
+    await page.waitForSelector('img')
+
+    // Navigate to other site
+    await page.goto('https://platzi.com/')
+    await page.waitForSelector('#home-public-old')
+
+    // Navigate back
+    await page.goBack()
+    await page.waitForSelector('img')
+
+    // Navigate forward
+    await page.goForward()
+
+    // Open new page
+    const page2 = await browser.newPage()
+    await page2.goto('https://github.com/')
+
+
     await browser.close()
 
-  }, 30000)
+  }, 350000)
 
 })
